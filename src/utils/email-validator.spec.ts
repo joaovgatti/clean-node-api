2efcf5ac230bdf9e9b3468a90e476c3da4b2fc7a
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-09-25 17:36:00 -0300

    refactor

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -8,23 +8,28 @@ import isEmail = validator.isEmail;
      }
  }))
 
+const makeSut = (): EmailValidatorAdpter => {
+     return new EmailValidatorAdpter()
+}
+
+
 
 
 describe('EmailValidator Adapter',() => {
     test("Should return false when the validator returns false",() => {
-        const sut = new EmailValidatorAdpter()
+        const sut = makeSut()
         jest.spyOn(validator,'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_mailgmail.com')
         expect(isValid).toBe(false)
 })
     test('should return true when the validator returns true',() =>{
-        const sut = new EmailValidatorAdpter()
+        const sut = makeSut()
         const isValid = sut.isValid('valid_mail@gmail.com')
         expect(isValid).toBe(true)
 
     })
     test('should call validator with correct email',() =>{
-        const sut = new EmailValidatorAdpter()
+        const sut = makeSut()
         const isEmailSpy = jest.spyOn(validator,"isEmail")
         sut.isValid('any_mail@email.com')
         expect(isEmailSpy).toHaveBeenCalledWith('any_mail@email.com')
