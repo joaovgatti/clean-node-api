16ccd4351ceddc451e24b08b36e842f5561c8cf2
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-09-25 17:19:16 -0300

    test/feat returns true when the emailvalidator returns true

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,10 +1,27 @@
 import {EmailValidatorAdpter} from "./email-validator";
+import validator from "validator";
+import isEmail = validator.isEmail;
+
+ jest.mock('validator',() => ({
+     isEmail (): boolean{
+         return true
+     }
+ }))
+
+
 
 describe('EmailValidator Adapter',() => {
     test("Should return false when the validator returns false",() => {
         const sut = new EmailValidatorAdpter()
-        const isValid = sut.isValid('invalid_mail@gmail.com')
+        jest.spyOn(validator,'isEmail').mockReturnValueOnce(false)
+        const isValid = sut.isValid('invalid_mailgmail.com')
         expect(isValid).toBe(false)
+})
+    test('should return true when the validator returns true',() =>{
+        const sut = new EmailValidatorAdpter()
+        const isValid = sut.isValid('valid_mail@gmail.com')
+        expect(isValid).toBe(true)
+
     })
 })
 
