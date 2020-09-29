8db70495e56ce69500013282f60b5e443c4da2dd
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-09-29 17:43:42 -0300

    refactor

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -86,6 +86,19 @@ describe('DbAddAccount Usecase',() => {
             password: 'hashed_password'
         })
     })
+    test('should throw if AddAccountRepository throws',async () => {
+        const {sut,addAccountRepositoryStub} = makeSut()
+        jest.spyOn(addAccountRepositoryStub,'add').
+        mockReturnValueOnce(new Promise((resolve,reject) => reject(new Error())))
+        const accoundData = {
+            name:'valid_name',
+            email:'valid_mail',
+            password: 'valid_password'
+        }
+        const promise = sut.add(accoundData)
+        await expect(promise).rejects.toThrow()
+    })
+
 })
 
 
