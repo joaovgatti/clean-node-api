4538db6701892424fec55ba11eb0cecd0b56fbe9
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-01 15:19:05 -0300

    m

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -6,6 +6,7 @@ import {AddAccountRepository} from "../../protocols/add-account-repository";
 export class DbAddAccount implements AddAccount{
     private readonly encrypter: Encrypter
     private readonly addAccountRepository: AddAccountRepository
+
     constructor(encrypter: Encrypter,addAccountRepository: AddAccountRepository) {
         this.encrypter = encrypter
         this.addAccountRepository = addAccountRepository
