03daa7dfb74935e8824384c14bbaf03851e853ff
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-01 15:32:00 -0300

    new test

--- src/infra/criptography/bcrypt-adapater.spec.ts
+++ src/infra/criptography/bcrypt-adapater.spec.ts
@@ -1,6 +1,14 @@
 import bcrypt from 'bcrypt'
 import {BcryptAdapter} from "./bcrypt-adapter";
 
+
+
+jest.mock('bcrypt',() => ({
+    async hash(): Promise<string>{
+        return new Promise(resolve => resolve('hash'))
+    }
+}))
+
 describe('Bcrypt Adapter',() => {
     test('should call Bcrypt with correct values', async() => {
         const salt = 12
@@ -9,5 +17,11 @@ describe('Bcrypt Adapter',() => {
         await sut.encrypt('any_value')
         expect(hashSpy).toHaveBeenCalledWith('any_value',salt)
 
+    })
+    test('should return hash on success ',async() => {
+        const salt = 12
+        const sut = new BcryptAdapter(salt)
+        const hash = await sut.encrypt('any_value')
+        expect(hash).toBe('hash')
     })
 })
