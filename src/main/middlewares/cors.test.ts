595ae043109a4ab4bc5616b088b4ec4feb35c888
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-02 17:35:52 -0300

    ensure requests return json as default content-type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,9 +3,9 @@ import request from "supertest";
 
 describe('CORS Middleware',() => {
      test('Should enable CORS',async () => {
-         app.get('/test_cors',((req, res) => {
+         app.get('/test_cors',(req, res) => {
              res.send()
-         }))
+         })
          await request(app)
              .get('/test_cors')
              .expect('access-control-allow-origin','*')
