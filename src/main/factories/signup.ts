484f7c956036cbdf9c5a1e4b99b02ced815cc6e4
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-05 16:16:26 -0300

    add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import {SignUpController} from "../../presentation/controllers/signup";
+import {EmailValidatorAdapter} from "../../utils/email-validator-adapter";
+import {DbAddAccount} from "../../data/usecases/add-account/db-add-account";
+import {BcryptAdapter} from "../../infra/criptography/bcrypt-adapter";
+import {AccountMongoRepository} from "../../infra/db/mongodb/account-repository/account";
+
+export const makeSignUpController = (): SignUpController => {
+    const salt = 12
+    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const accountMongoRepository = new AccountMongoRepository()
+    const bcrypterAdapter = new BcryptAdapter(salt)
+    const dbAddAccount = new DbAddAccount(bcrypterAdapter,accountMongoRepository)
+    return new SignUpController(emailValidatorAdapter,dbAddAccount)
+
+}
