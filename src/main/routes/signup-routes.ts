e22b656f296ec05273c1ab9db22271bc5e170f97
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-05 14:03:55 -0300

    refactor

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,9 @@
-import {Router} from "express";
+import { Router } from 'express'
 
 export default (router: Router): void => {
-    router.post('/signup',(req, res) => {
+        router.post('/signup',(req, res) => {
         res.json({ok:'ok'})
     })
 }
+
+
