e91a29c700455aa7eacde7a327eae55c289ca4d4
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-08 10:50:47 -0300

    refactor: create makeSut factory

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -1,10 +1,26 @@
 import {LogInController} from "./login";
 import {badRequest} from "../helpers/http-helper";
 import {MissingParamError} from "../errors/errors";
+import {SignUpController} from "./signup";
+
+
+interface SutTypes{
+    sut: LogInController
+}
+
+const  makeSut = (): SutTypes => {
+    const sut = new LogInController()
+    return {
+        sut
+    }
+}
+
+
+
 
 describe('Login Controller', () => {
     test('should return 400 if no email is provided',async () => {
-        const sut = new LogInController()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 password:'any_password'
@@ -15,7 +31,7 @@ describe('Login Controller', () => {
 
     });
     test('should return 400 if no password if provided',async() => {
-        const sut = new LogInController()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 email:'any_email@mail.com'
