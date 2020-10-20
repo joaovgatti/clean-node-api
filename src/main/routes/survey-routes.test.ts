38fd7f897451b5212e30e61903c54921c1ab1612
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2020-10-20 16:39:12 -0300

    middlewares

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import app from "../config/app";
+import {MongoHelper} from "../../infra/db/mongodb/helpers/mongo-helper";
+import {Collection} from "mongodb";
+
+
+
+let SurveyCollection: Collection
+
+describe('Login Routes',() => {
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        SurveyCollection = await MongoHelper.getCollection('surveys')
+        await SurveyCollection.deleteMany({})
+    })
+
+
+    describe('POST /surveys',() => {
+        test('should return 204 on addSurvey success', async () => {
+               request(app)
+                .post('/api/surveys')
+                .send({
+                   question:'question',
+                   answers: [{
+                        answers:'answer 1',
+                        image:'image'
+                   },{
+                       answers: 'answers 2'
+                   }]
+                })
+                .expect(204)
+        })
+    })
+})
--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -6,6 +6,15 @@ import {LoadAccountByToken} from "../../domain/usecases/load-account-by-token";
 import {AccountModel} from "../../domain/models/account";
 
 
+
+const makeFakeRequest = (): HttpRequest => ({
+    headers:{
+        'x-access-token':'any_token'
+    }
+})
+
+
+
 const makeFakeAccount = (): AccountModel => ({
     id:'valid_id',
     name:'valid_name',
@@ -13,28 +22,50 @@ const makeFakeAccount = (): AccountModel => ({
     password:'hashed_password'
 })
 
+const makeLoadAccountByTokenStub = (): LoadAccountByToken =>{
+    class LoadAccountByTokenStub implements LoadAccountByToken{
+        async load(accessToken:string, role?:string): Promise<AccountModel>{
+            return new Promise(resolve => resolve(makeFakeAccount()))
+        }
+    }
+    return new LoadAccountByTokenStub()
+}
+
+interface SutTypes {
+    loadAccountByTokenStub: LoadAccountByToken
+    sut: AuthMiddleware
+}
+
+
+const makeSut = (): SutTypes => {
+    const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    return ({
+        loadAccountByTokenStub,
+        sut
+    })
+}
+
 
 describe('auth middleware',() => {
     test('should return 403 if no x-access-token exists in headers',async () => {
-        const sut = new AuthMiddleware()
+        const {sut} = makeSut()
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
     })
-    test('should call LoadAccountByToken with correct accessToken',async () => {
-        class LoadAccountByTokenStub implements LoadAccountByToken{
-            async load(accessToken: string, role?: string): Promise<AccountModel>{
-                return new Promise(resolve => resolve(makeFakeAccount()))
-            }
-        }
 
-        const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    test('should call LoadAccountByToken with correct accessToken',async () => {
+        const {loadAccountByTokenStub,sut} = makeSut()
         const loadSpy = jest.spyOn(loadAccountByTokenStub,'load')
-        const sut = new AuthMiddleware(loadAccountByTokenStub)
-        await sut.handle({
-            headers:{
-                'x-access-token':'any_token'
-            }
-        })
+        await sut.handle(makeFakeRequest())
+        expect(loadSpy).toHaveBeenCalledWith('any_token')
+    })
+
+    test('should return 403 if LoadAccountByToken returns null',async () => {
+        const {loadAccountByTokenStub,sut} = makeSut()
+        jest.spyOn(loadAccountByTokenStub,'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+        const promise = await sut.handle(makeFakeRequest())
+        expect(promise).toEqual(forbidden(new AccessDeniedError()))
     })
 
 })
